7f6b0520d9c71906949e5ac2357f764c9aa5a47a
Tristan <tristansf@outlook.com>
2024-10-29 23:07:42 -0400

    Made the main method and added comments

---
+++ Driver.java
@@ -0,0 +1,11 @@
+/**
+ * Driver
+ */
+public class Driver {
+    public static void main(String[] args) {
+        // TODO: add the main loop in here, and my reccomendation is to have a switch case and based on the option, we call a method from a seperate class
+
+        // also, if there are methods we all are planning on using (im not sure what yet, this is just primary), we should have a seperate class/interface from which each new class can either inherit from the superclass or use the interface.
+            // it'll keep the stuff more seperate and make things easier to manage.
+    }
+}
